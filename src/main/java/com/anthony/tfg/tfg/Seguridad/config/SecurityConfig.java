5d236a581b3465060500c9a8c77914f59d826fed
tonycode2 <velurn123@gmail.com>
2025-12-27 20:44:36 -0600

    Disable CORS in security configuration for authentication endpoints

--- src/main/java/com/anthony/tfg/tfg/Seguridad/config/SecurityConfig.java
+++ src/main/java/com/anthony/tfg/tfg/Seguridad/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(authRequest -> authRequest.requestMatchers("/auth/register", "/auth/login")
                         .permitAll()
                         .anyRequest()
