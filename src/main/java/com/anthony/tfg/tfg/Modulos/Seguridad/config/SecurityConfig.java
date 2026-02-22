7ebcff8baf94c0a917234eb54c1dce33fadd7092
tonycode2 <velurn123@gmail.com>
2026-02-21 22:50:55 -0600

    Add additional allowed origin for CORS in SecurityConfig
    - Updated CORS configuration to allow requests from http://localhost:4173 in addition to http://localhost:5173.

--- src/main/java/com/anthony/tfg/tfg/Modulos/Seguridad/config/SecurityConfig.java
+++ src/main/java/com/anthony/tfg/tfg/Modulos/Seguridad/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("http://localhost:5173", "http://localhost:4173")
         );
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
